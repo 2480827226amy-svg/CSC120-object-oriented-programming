4b2b0297ab2c01ce309f1a8ab651e7265a9c3add
2480827226amy-svg <2480827226amy@gmail.com>
2026-02-26 12:54:27 -0500

    Add files via upload

---
+++ CSC120-A8 2/Library.java
@@ -0,0 +1,197 @@
+/* This is a stub for the Library class */
+
+import java.util.Hashtable;
+import java.util.Map;
+/**
+ * A Library is a Building that manages a collection of book titles and their availability.
+ * The collection maps a title to a Boolean where true = available, false = checked out.
+ */
+public class Library extends Building implements LibraryRequirements {
+  /** Map from book title to availability. */
+  private Hashtable<String, Boolean> collection;
+  private final boolean hasElevator;
+
+// Constructor
+  /**
+   * Construct a Library.
+   * @param name     library name
+   * @param address  street address
+   * @param nFloors  number of floors (>= 1)
+   */
+  public Library(String name, String address, int nFloors, boolean hasElevator) {
+    super(name, address, nFloors);
+    this.hasElevator = hasElevator;
+    this.collection = new Hashtable<String, Boolean>();
+    System.out.println("You have built a library: 📖");
+  }
+
+  /**
+   * Overloaded constructor : Construct a Library with elevator by default.
+   * @param name
+   * @param address
+   * @param nFloors
+   */
+  public Library(String name, String address, int nFloors) {
+    this(name, address, nFloors, true);
+  }
+
+  /**
+   * Overloaded constructor : Construct a Library with 1 floor and elevator by default.
+   * @param name
+   * @param address
+   */
+  public Library(String name, String address) {
+    this(name, address, 1, true);
+  }
+
+  /** Add a title to the collection, marked available. */
+  public void addTitle(String title){
+      collection.put(title, true);
+  }
+
+  /**
+   * Remove a title from the collection.
+   * @param title the title to remove
+   * @return the removed title
+   */
+  public String removeTitle(String title){
+    if(collection.containsKey(title)){
+      collection.remove(title);
+      return title;
+    }else{
+      throw new IllegalArgumentException(title + "not found in the collection.");
+    }
+  }
+  /**
+   * Check out a title from the collection.
+   * @param title the title to check out
+   */
+  public void checkOut(String title){
+    if(collection.containsKey(title)){
+      if(collection.get(title)){
+        collection.put(title, false);
+      }else{
+        throw new IllegalArgumentException(title + "has been checked out.");
+      }
+    }else{
+      throw new IllegalArgumentException(title + "is not in the collection.");
+    }
+  }
+
+  /**
+   * Return a title to the collection.
+   * @param title the title to return
+   */
+  public void returnBook(String title){
+    if(collection.containsKey(title)){
+      if(!collection.get(title)){
+        collection.put(title,true);
+      }else{
+        throw new IllegalArgumentException(title + "is in the collection already.");
+      }
+    }else{
+      throw new IllegalArgumentException(title + "is not in the collection.");
+    }
+  }
+
+  /**
+   * Check if the collection contains a title.
+   * @param title the title to check
+   * @return true if the title is in the collection, false otherwise
+   */
+  public boolean containsTitle(String title){
+    return collection.containsKey(title);
+  }
+  
+  /**
+   * Check if a title is available.
+   * @param title the title to check
+   * @return true if the title is available, false otherwise
+   */
+  public boolean isAvailable(String title){
+    return collection.getOrDefault(title, false);
+  }
+
+  /**
+   * Print the entire collection with availability status.
+   */
+  public void printCollection(){
+    System.out.println("Library Collection: \n");
+    for (Map.Entry<String, Boolean> entry : collection.entrySet()) {
+      if(entry.getValue()){
+        System.out.println("Title: "+ entry.getKey()+ "; Status: Available" );
+      }else{
+        System.out.println("Title: "+ entry.getKey()+ "; Status: Checked Out" );
+      }
+    }
+  }
+
+// Overriden Methods
+  /**
+   * Show the options available in a library.
+   * print the options to the terminal.
+   */
+  public void showOptions() {
+    System.out.println("Available options at " + this.name + ":\n" + " + enter()\n" + " + exit()\n" + " + goUp()\n" + " + goDown()\n" + " + goToFloor(n)\n" + " + addTitle(String)\n" + " + removeTitle(String)\n" + " + checkOut(String)\n" + " + returnBook(String)\n" + " + containsTitle(String)\n" + " + isAvailable(String)\n" + " + printCollection()");
+  }
+
+  /**
+   * Override goToFloor method to add elevator funcationality.
+   * @param floorNum the floor numbe to go to
+   * @throws RuntimeException if floorNum is invalid or elevator rules are violated
+   */
+  public void goToFloor(int floorNum) {
+    if (this.activeFloor == -1) {
+      throw new RuntimeException("You are not inside this Library. Must call enter() first.");
+    }
+    if (floorNum < 1 || floorNum > this.nFloors) {
+      throw new RuntimeException("Invalid floor number. Valid range for this Library is 1-" + this.nFloors + ".");
+    }
+    if (hasElevator) {
+      System.out.println("You take the elevator to floor #" + floorNum + ".");
+      this.activeFloor = floorNum;
+    } else {
+        if (Math.abs(floorNum - this.activeFloor) > 1) {
+          throw new RuntimeException("No elevator: you can only move one floor at a time.");
+        } else {
+            System.out.println("You walk to floor #" + floorNum + ".");
+            this.activeFloor = floorNum;
+        }
+    }
+  }
+
+
+public static void main(String[] args) {
+    System.out.println("------------------------------------");
+    System.out.println("Test of Library constructor/methods");
+    System.out.println("------------------------------------");
+
+    Library neilson = new Library("Neilson Library", "7 Neilson Drive", 5);
+    System.out.println(neilson);
+    neilson.showOptions();
+
+    System.out.println("-----------------------------------");
+    System.out.println("Demonstrating add/remove/checkout");
+    System.out.println("-----------------------------------");
+    neilson.addTitle("CSC 120 Textbook");
+    neilson.addTitle("Design as Art");
+    neilson.printCollection();
+    neilson.checkOut("Design as Art");
+    neilson.printCollection();
+    neilson.returnBook("Design as Art");
+    neilson.removeTitle("CSC 120 Textbook");
+    neilson.printCollection();
+
+    System.out.println("-----------------------------------");
+    System.out.println("Demonstrating enter/exit/navigation");
+    System.out.println("-----------------------------------");
+    neilson.enter();
+    neilson.goToFloor(3);
+    neilson.goToFloor(5);
+    neilson.exit();
+
+    System.out.println("-----------------------------------");
+    System.out.println("All tests finished.");
+    System.out.println("-----------------------------------");
+    }
+}
---
+++ CSC120-A8 2/reflection.md
@@ -0,0 +1,16 @@
+Use this file to record your reflection on this assignment.
+
+- Which methods did you decide to `overload`, and why?
+In the Cafe class, the constructor is overloaded, in order to make the program run smoothly even there are not corresponding values for the input parameters. We choose to set the parameters to default values. 
+
+The sellCoffee method is overloaded as well, because consumers do not always order for specific amount of sugar packets and creams. To ensure the program runs, we will set the default amount of sugar packets and creams. 
+
+The restock method is overloaded to ensure that even if the barista do not enter the specific amount refilled, the program sets the restock value to maximum automatically.
+
+The shopOptions method is overloaded to add additional methods specific to cafe class. 
+
+- What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I would advice starting by understanding what overriding and overloading means in Java. And then, start with the constructor of cafe class and experiment with adding or removing parameters in the input, which can help students gain a practical idea of how to overload a code chunk. 
+
+We spent a lot of time figuring out how to start. From the classes, we understood what override and overload basically means, but when writing the codes, we found it hard to start. Therefore, a piece of advice I would give to future students is to start by reviewing in class examples. 
